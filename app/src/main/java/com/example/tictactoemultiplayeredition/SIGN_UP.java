4d2a7690bc1754a65927ddc9665a31fdf5e674a9
georgebadekas <81552322+georgebadekas@users.noreply.github.com>
2023-08-01 13:52:37 +0300

    finished with Firebase auth on SIGN_UP function, (also sets a username) , i still have to perform the query to check if the username already exist on the firestore usernames collection

--- app/src/main/java/com/example/tictactoemultiplayeredition/SIGN_UP.java
+++ app/src/main/java/com/example/tictactoemultiplayeredition/SIGN_UP.java
@@ -1,24 +1,114 @@
 package com.example.tictactoemultiplayeredition;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.tictactoemultiplayeredition.USER;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SIGN_UP extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getSupportActionBar().hide();
-        final ImageButton back_button = (ImageButton) findViewById(R.id.back_button);
+        FirebaseApp.initializeApp(this);
+        FirebaseAuth auth;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+        EditText email_field = findViewById(R.id.email_field);
+        EditText username_field = findViewById(R.id.username_field);
+        EditText password_field = findViewById(R.id.password_field);
+        EditText confirm_password_field = findViewById(R.id.confirm_password_field);
+        TextView error_textview = findViewById(R.id.error);
+        error_textview.setVisibility(View.GONE);
+
+
+
+        ImageButton  sing_up = findViewById(R.id.sign_up);
+            //Intent secondActivityIntent = new Intent(this, MainScreen.class);
+        sing_up.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println(password_field.getText().toString());
+                //startActivity(secondActivityIntent);
+                boolean flag = true;
+                boolean flag_password = password_field.getText().toString().equals(confirm_password_field.getText().toString());
+                String email = email_field.getText().toString();
+                String password = password_field.getText().toString();
+                String username = username_field.getText().toString();
+                if(flag_password){
+
+                }else{
+                    error_textview.setText("Passwords don't much");
+                    error_textview.setVisibility(View.VISIBLE);
+                    flag = false;
+                }
+                String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+                Pattern pattern = Pattern.compile(emailRegex);
+                Matcher matcher = pattern.matcher(email);
+                if(matcher.matches()){
+
+                }else{
+                    error_textview.setText("email is not valid");
+                    error_textview.setVisibility(View.VISIBLE);
+                    flag = false;
+                }
+                if(flag){
+                    sign_up_user(auth,email,password,username,db);
+                }
+
+
+            }
+        });
+
+
     }
+
+    void sign_up_user(FirebaseAuth auth,String email, String password,String username,FirebaseFirestore db){
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()){
+                FirebaseUser user = auth.getCurrentUser();
+                String userId = user.getUid();
+                USER newUser = new USER(userId,email,username);
+                db.collection("usernames").document(userId).set(newUser);
+                Intent main_sceen_intent = new Intent(this, MainScreen.class);
+                startActivity(main_sceen_intent);
+
+
+
+            }else{
+                System.out.println("Problem signing up");
+            }
+        });
+    }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/tictactoemultiplayeredition/USER.java
@@ -0,0 +1,38 @@
+package com.example.tictactoemultiplayeredition;
+
+public class USER {
+    String user_id;
+    String email;
+    String username;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public USER(String user_id, String email, String username) {
+        this.user_id = user_id;
+        this.email = email;
+        this.username = username;
+    }
+
+}
